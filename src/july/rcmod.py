c85beb50a857b7575b276f1368093746c87e2e93
michael <iptq@users.noreply.github.com>
2024-05-06 15:27:45 -0500

    Update rcmod.py

--- src/july/rcmod.py
+++ src/july/rcmod.py
@@ -51,4 +51,5 @@ def update_rcparams(
     rcmod["figure.dpi"] = dpi
     rcmod.update(rc_params_dict)
     with warnings.catch_warnings():
+        warnings.simplefilter("ignore", mpl.MatplotlibDeprecationWarning)
         mpl.rcParams.update(rcmod)
